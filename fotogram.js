b01e757156a87f7b17aed735e6ddc01d193d8b8b
Elton57 <kjerimovikje@gmail.com>
2025-11-04 21:41:22 +0100

    next & prev button add

--- fotogram.js
+++ fotogram.js
@@ -2,6 +2,10 @@ const dialogRef = document.getElementById('dialog-box');
 const imgGallery = document.getElementById('imgGallery');
 const imgBox = document.querySelector('.imgBox');
 const closeBtn = document.getElementById('close-btn');
+const backBtn = document.getElementById('back-btn');
+const nextBtn = document.getElementById('next-btn');
+const counter = document.getElementById('counter');
+const filename = document.getElementById('filename');
 
 let myImgs = [
     'amman.jpg',
@@ -24,21 +28,46 @@ let myImgs = [
     'zurich.jpg',
 ];
 
+let currentIndex = 0;
 
-function openDialog(imgName){
+
+function openDialog(imgName) {
     dialogRef.showModal();
-   
-        imgBox.innerHTML = getNotesHtml(imgName);
-    
+
+    imgBox.innerHTML = getNotesHtml(imgName);
+
 }
 
-function getNotesHtml(myImgs){
+function getNotesHtml(myImgs) {
     return `    <div class= "dialogGallery">
                         <img class="dialogGallery" src="./img/${myImgs}" alt="${myImgs}">
                  </div>`
 }
 
 
-function closeDialog(){
+function closeDialog() {
     dialogRef.close();
 }
+
+function updateDialogImage() {
+    const currentFile = myImgs[currentIndex];
+    imgBox.innerHTML = getNotesHtml(myImgs[currentIndex]);
+    if (counter) {
+        counter.textContent = `${currentIndex + 1} / ${myImgs.length}`;
+    }
+    if (filename) {
+        filename.textContent = currentFile;
+    }
+
+}
+
+function prev() {
+    currentIndex = (currentIndex - 1 + myImgs.length) % myImgs.length;
+    updateDialogImage();
+}
+
+function next() {
+    currentIndex = (currentIndex + 1) % myImgs.length;
+    updateDialogImage();
+}
+
